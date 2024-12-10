8041952849a43e02c6104740934d7b3109f7928d
Carlos <carloscruzp06@gmail.com>
2024-12-10 07:22:44 +0100

    [FileReader] Add convenience method to read files

--- src/main/java/me/advent/tools/FileReader.kt
+++ src/main/java/me/advent/tools/FileReader.kt
@@ -7,9 +7,17 @@ class FileReader {
         /**
          * Read the contents of a file.
          *
-         * [filename] name of the file to be read.
+         * filename name of the file to be read.
          * @return content of the file.
          */
         fun readFile(filename: String) = File(filename).readText()
+
+        /**
+         * Get the contents of a file split by lines.
+         *
+         * @param filename name of the file to be read.
+         * @return list that contains the content read split by line.
+         */
+        fun readLines(filename: String) = readFile(filename).split('\n')
     }
 }
